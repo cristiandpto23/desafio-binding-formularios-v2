ab391d9e8e1fba83076d2d67ab7496b77175ebf2
cristiandpto23 <cristian.web.development@gmail.com>
2024-10-06 23:13:39 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import creditCard from './components/creditCard.vue'
+</script>
+
+<template>
+  <creditCard />
+</template>
+
+<style scoped>
+  body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 60vh;
+    font-size: 10px;
+    font-family: sans-serif;
+  }
+</style>
---
+++ src/components/creditCard.vue
@@ -0,0 +1,153 @@
+<script>
+export default {
+    name: 'creditCard',
+    data() {
+        return {
+            usuario: {
+                titulo: '',
+                // Visa Classic Crédito
+                chip: '',
+                // /chip.png
+                numero: '',
+                // 400 500 600 700
+                fecha: '',
+                // 01/30
+                propietario: '',
+                // William Henry Gates III
+                tipo: ''
+                // visa.png
+            }
+        }
+    }
+}
+</script>
+
+<template>
+    <body>
+        <form>
+        <div>
+            <label>Título de la tarjeta: </label>
+            <input v-model="usuario.titulo" />
+        </div>
+
+        <div>
+            <label>Chip SRC: </label>
+            <input v-model="usuario.chip" />
+        </div>
+
+        <div>
+            <label>Número: </label>
+            <input v-model="usuario.numero" />
+        </div>
+
+        <div>
+            <label>Fecha de expiración: </label>
+            <input v-model="usuario.fecha"/>
+        </div>
+
+        <div>
+            <label>Propietario: </label>
+            <input v-model="usuario.propietario" />
+        </div>
+
+        <div>
+            <label>Tipo de tarjeta SRC: </label>
+            <input v-model="usuario.tipo" />
+        </div>
+        </form>
+
+        <div class="carnet">
+        <h3> {{ usuario.titulo }} </h3>
+        <img width="40" :src="'../src/assets/img' + usuario.chip" alt="" />
+        <div>
+            <h2> {{ usuario.numero }}</h2>
+            <span>Fecha Exp: <b>01/30</b></span>
+        </div>
+        <footer>
+            <span> {{ usuario.propietario }}</span>
+            <img v-bind:src="'../src/assets/img' + usuario.tipo" width="60" />
+        </footer>
+        </div>
+    </body>
+</template>
+
+<style scoped>
+    * {
+    margin: 0;
+    }
+    
+    body {
+    display: flex;
+    gap: 40px;
+    justify-content: center;
+    align-items: center;
+    height: 60vh;
+    font-size: 10px;
+    font-family: sans-serif;
+    }
+
+    #app {
+    display: flex;
+    gap: 40px;
+    }
+    form {
+    padding: 18px;
+    border-radius: 20px;
+    border: ridge 1px;
+    box-shadow: 1px 1px 1px 1px gray;
+    font-weight: bold;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    justify-content: center;
+    }
+    form div {
+    display: grid;
+    grid-template-columns: 100px 200px;
+    align-items: center;
+    gap: 5px;
+    }
+
+    form div label {
+    text-align: right;
+    }
+
+    .carnet {
+    width: 400px;
+    height: 230px;
+    background: #1c1a1e;
+    color: white;
+    border-radius: 20px;
+    padding: 20px;
+    box-shadow: 0px 0px 10px 2px black;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    }
+
+    h3 {
+    font-size: 20px;
+    }
+
+    h2 {
+    font-size: 30px;
+    letter-spacing: 10px;
+    margin-bottom: 15px;
+    text-shadow: 0px 0px 1px gold;
+    font-family: Times;
+    }
+
+    b {
+    font-size: 16px;
+    }
+
+    footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    font-weight: bold;
+    font-size: 18px;
+    text-transform: uppercase;
+    font-style: italic;
+    }
+</style>
